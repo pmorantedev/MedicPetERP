019ab0a255963e0fe8883c3101a91b66c6a4397a
txell <txell@txell-VirtualBox>
2023-03-16 20:14:59 +0100

    2023/03/16 -> Plantilles HTML (v.1) + Controller (Client) - Plantilla base pel llistat i formulari (crear i modificar dades d'un client) - Creats mètodes i mappings per les rutes del CRUD

---
+++ src/main/java/com/gruptd/medicPet/controllers/ClientController.java
@@ -0,0 +1,54 @@
+package com.gruptd.medicPet.controllers;
+
+import com.gruptd.medicPet.models.Client;
+import com.gruptd.medicPet.services.ClientServices;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ClientController {
+
+    @Autowired
+    private ClientServices clientService;
+    
+    @GetMapping("/medicpet/clients")
+    public String principalClients(Model model) {
+        log.info("Executant el controlador de clients");
+        Iterable<Client> clients = clientService.findAllClients();
+        
+        model.addAttribute("clients", clients);
+        
+        return "clientsMain";
+    }
+    
+    @GetMapping("/medicpet/clients/fitxa")                                      // URL fitxa client (FORM)
+    public String fitxaClient(Client client) {
+        
+        return "clientForm";
+    }
+    
+    @PostMapping("/medicpet/clients/desar")                                     // URL 'CREATE' client (FORM)
+    public String desarClient(Client client){
+        clientService.saveClients(client);
+        return "redirect:/medicpet/clients";
+    }
+    
+    @PostMapping("/medicpet/clients/fitxa/{idclient}")                                // URL 'WRITE' client (FORM)
+    public String modificarClient(Client client, Model model) {
+        client = clientService.getClients(client);
+        model.addAttribute("client", client );
+        return "clientForm";
+    }
+    
+    @PostMapping("/medicpet/clients/eliminar/{idclient}")                             // URL 'DELETE' client (FORM)
+    public String eliminar(Client client){
+        clientService.deleteClients(client);
+        return "redirect:/medicpet/clients";
+    }
+    
+}
