85bbc942c020fabb3bd0c4e0048d38ebaabc5f6b
pmorante <pmorante@mga.es>
2023-03-16 09:55:21 +0100

    Logout funcional / protecció contra atacs CSRF

--- src/main/java/com/gruptd/medicPet/config/WebSecurityConfig.java
+++ src/main/java/com/gruptd/medicPet/config/WebSecurityConfig.java
@@ -7,12 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,17 +24,19 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/medicpet/**").hasRole("USER")
-                .anyRequest().anonymous()
+                .requestMatchers("/**").permitAll()
                 )
                 .formLogin((form) -> form
                 .loginPage("/login")
                 .permitAll()
                 .defaultSuccessUrl("/medicpet/tractaments", true)
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout"))
+                .csrf();
 
         return http.build();
     }
@@ -52,16 +50,4 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user
-//                = User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
 }
