c8007c18b9be7a4257abcf246cb370bc7df2532b
izan <izan@izan-VirtualBox>
2023-03-30 16:50:14 +0200

    30-03-23 -> Cercador Tractaments

--- src/main/java/com/gruptd/medicPet/controllers/TractamentController.java
+++ src/main/java/com/gruptd/medicPet/controllers/TractamentController.java
@@ -3,14 +3,19 @@ package com.gruptd.medicPet.controllers;
 import com.gruptd.medicPet.services.TractamentServices;
 import com.gruptd.medicPet.models.Tractament;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
@@ -19,13 +24,27 @@ public class TractamentController {
     @Autowired
     private TractamentServices tractamentService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/medicpet/tractaments")
-    public String principalTractament(Model model) {
-        Iterable<Tractament> tractaments = tractamentService.findAll();
+    public String principalTractament(Model model, @Param("paraulaClau") String paraulaClau) {
+
+        Iterable<Tractament> tractaments;
+        
+        // String paraulaClau = "TRACTAMENT";
+        if (paraulaClau != null) {
+            String sql = "SELECT * FROM tractament t WHERE CONCAT(t.nom, t.id, t.preu) LIKE '%" + paraulaClau + "%'";
+            tractaments = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Tractament.class));
+        } else {
+            tractaments = tractamentService.findAll();
+        }
+        
         model.addAttribute("tractaments", tractaments);
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
         model.addAttribute("userName", username);
-        
+
         return "tractamentsMain";
     }
 
@@ -35,7 +54,7 @@ public class TractamentController {
         model.addAttribute("tractament", tractament);
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         model.addAttribute("userName", username);
-        
+
         return "tractamentsForm";
     }
 
@@ -43,7 +62,7 @@ public class TractamentController {
     public String fitxaTractament(Tractament tractament, Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         model.addAttribute("userName", username);
-        
+
         return "tractamentsForm";
     }
 
