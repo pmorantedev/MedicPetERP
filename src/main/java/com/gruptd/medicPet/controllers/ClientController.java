8be068f77f0622b43c22f5e44937855f22bbce09
txell <txell@txell-VirtualBox>
2023-03-31 18:16:39 +0200

    2023/03/31 -> Modal Bootstrap elimina (Provisional) - Elimina sempre el primer registre de la taula...

--- src/main/java/com/gruptd/medicPet/controllers/ClientController.java
+++ src/main/java/com/gruptd/medicPet/controllers/ClientController.java
@@ -21,86 +21,85 @@ public class ClientController {
 
     @Autowired
     private ClientServices clientService;
-    
+
     @Autowired
     private MascotaServices mascotaService;
-    
+
     @GetMapping("/medicpet/clients")
     public String principalClients(Model model) {                               // URL 'READ' clients (LIST)
         log.info("Executant controlador clients: LLISTAT");
         Iterable<Client> clients = clientService.findAll();
         Iterable<Mascota> mascotes = mascotaService.findAll();
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        
+
         model.addAttribute("clients", clients);
         model.addAttribute("mascotes", mascotes);
         model.addAttribute("userName", username);
         model.addAttribute("pagina", "Clients");
-        
+
         return "clientsMain";
     }
-    
+
     @GetMapping("/medicpet/clients/fitxa")                                      // URL mostrar fitxa client (FORM)
     public String crearClient(Client client, Model model) {
-        
+
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         model.addAttribute("userName", username);
         model.addAttribute("pagina", "Clients");
-        
+
         log.info("Executant controlador clients: FITXA NOU CLIENT...");
-        
+
         return "clientForm";
     }
-    
+
     @PostMapping("/medicpet/clients/desar")                                     // URL 'CREATE' / 'UPDATE' client (FORM)
-    public String desarClient(@Valid Client client, Errors errors){
-        
-        log.info("Executant controlador clients: VALIDANT DADES CLIENT...");        
-        if(errors.hasErrors()){ // Si s'han produït errors...
-             return "clientForm"; // Mantenim l'usuari a la pàgina del formulari
+    public String desarClient(@Valid Client client, Errors errors) {
+
+        log.info("Executant controlador clients: VALIDANT DADES CLIENT...");
+        if (errors.hasErrors()) { // Si s'han produït errors...
+            return "clientForm"; // Mantenim l'usuari a la pàgina del formulari
         }
-        
-        if ( client.getIdclient() == null ) {
+
+        if (client.getIdclient() == null) {
             clientService.save(client);
-            log.info("Executant controlador clients: NOU CLIENT DESAT ( ID:"+client.getIdclient()+", "+client.getNomComplert()+" )...");
+            log.info("Executant controlador clients: NOU CLIENT DESAT ( ID:" + client.getIdclient() + ", " + client.getNomComplert() + " )...");
         } else {
             clientService.update(client);
-            log.info("Executant controlador clients: CLIENT ACTUALITZAT ( ID:"+client.getIdclient()+", "+client.getNomComplert()+" )...");
+            log.info("Executant controlador clients: CLIENT ACTUALITZAT ( ID:" + client.getIdclient() + ", " + client.getNomComplert() + " )...");
         }
-        
+
         return "redirect:/medicpet/clients";
     }
-    
+
     @GetMapping("/medicpet/clients/fitxa/{idclient}")                           // URL 'EDIT' client (FORM) ****
     public String modificarClient(Client client, Model model) {
-        
+
         client = clientService.getOne(client.getIdclient());
-        model.addAttribute("client", client );
-        log.info("Executant controlador clients: MOSTRAR FITXA CLIENT EXISTENT ( ID:"+client.getIdclient()+", "+client.getNomComplert()+" )...");
-        
+        model.addAttribute("client", client);
+        log.info("Executant controlador clients: MOSTRAR FITXA CLIENT EXISTENT ( ID:" + client.getIdclient() + ", " + client.getNomComplert() + " )...");
+
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         model.addAttribute("userName", username);
         model.addAttribute("pagina", "Clients");
-                
+
         return "clientForm";
     }
-    
+
     @PostMapping("/medicpet/clients/eliminar/{idclient}")                       // URL 'DELETE' client (FORM)
-    public String eliminar(Client client){        
-        
+    public String eliminar(Client client) {
+
         // TO-DO: detectar si hi havia mascotes associades i llistar-les al log
 //        if (!client.getMascotes().isEmpty()) {
 //            for(int i = 0; i<client.getMascotes().size(); i++)
 //                log.info("Executant el controlador de clients: CLIENT ("+client.getIdclient()+") / MASCOTA ASSOCIADA ("+client.getMascotes().get(i).getId_mascota()+") ELIMINADA");
 //        }
-    
 //        client = clientService.getOne(idclient);
 //        model.addAttribute("client", client );
-         
+
+        log.info("Executant controlador clients: CLIENT ELIMINAT ( ID:" + client.getIdclient() + ", " + client.getNomComplert() + " )...");
         clientService.delete(client);
-        log.info("Executant controlador clients: CLIENT ELIMINAT ( ID:"+client.getIdclient()+", "+client.getNomComplert()+" )...");
-        
+
         return "redirect:/medicpet/clients";
     }
-    
+
 }
