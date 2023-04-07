2c95741530eac5e223377872a22c54d0a4fdba0d
morante.garcia.pablo <morante.garcia.pablo@alumnat.copernic.cat>
2023-04-07 16:23:07 +0200

    validacions mascota/client camps buits

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -24,68 +24,111 @@ function validar() {
     var valid = true;
     var nomComplert = document.getElementById('nomComplert').value;
     var nomComplertError = document.getElementById('error-nom');
+    var nomBuitError = document.getElementById('errorBuit-nom');
 
     var dni = document.getElementById('dni').value;
     var dniError = document.getElementById('error-dni');
+    var dniBuitError = document.getElementById('errorBuit-dni');
 
     var telefon = document.getElementById('telefon').value;
     var telefonErrorLlarg = document.getElementById('error-telefonLlarg');
     var telefonErrorCar = document.getElementById('error-telefonCar');
+    var telefonBuitError = document.getElementById('errorBuit-telefon');
 
     var email = document.getElementById('email').value;
     var emailError = document.getElementById('error-email');
+    var emailBuitError = document.getElementById('errorBuit-email');
 
     var address = document.getElementById('address').value;
     var addressError = document.getElementById('error-address');
+    var addressBuitError = document.getElementById('errorBuit-address');
 
     if (nomComplert.length > 45) {
         valid = false;
         nomComplertError.style.display = 'block';
+        nomBuitError.style.display = 'none';
+        document.getElementById('nomComplert').classList.add('border-danger');
+    } else if (nomComplert.trim() === '' || nomComplert.length === 0) {
+        valid = false;
+        nomComplertError.style.display = 'none';
+        nomBuitError.style.display = 'block';
         document.getElementById('nomComplert').classList.add('border-danger');
     } else {
         nomComplertError.style.display = 'none';
+        nomBuitError.style.display = 'none';
         document.getElementById('nomComplert').classList.remove('border-danger');
     }
 
     if (dni.length > 9) {
         valid = false;
         dniError.style.display = 'block';
+        dniBuitError.style.display = 'none';
+        document.getElementById('dni').classList.add('border-danger');
+    } else if (dni.trim() === '' || dni.length === 0) {
+        valid = false;
+        dniError.style.display = 'none';
+        dniBuitError.style.display = 'block';
         document.getElementById('dni').classList.add('border-danger');
     } else {
         dniError.style.display = 'none';
+        dniBuitError.style.display = 'none';
         document.getElementById('dni').classList.remove('border-danger');
     }
 
     if (email.length > 45) {
         valid = false;
         emailError.style.display = 'block';
+        emailBuitError.style.display = 'none';
+        document.getElementById('email').classList.add('border-danger');
+    } else if (email.trim() === '' || email.length === 0) {
+        valid = false;
+        emailError.style.display = 'none';
+        emailBuitError.style.display = 'block';
         document.getElementById('email').classList.add('border-danger');
     } else {
         emailError.style.display = 'none';
+        emailBuitError.style.display = 'none';
         document.getElementById('email').classList.remove('border-danger');
     }
 
     if (address.length > 100) {
         valid = false;
         addressError.style.display = 'block';
+        addressBuitError.style.display = 'none';
+        document.getElementById('address').classList.add('border-danger');
+    } else if (address.trim() === '' || address.length === 0) {
+        valid = false;
+        addressError.style.display = 'none';
+        addressBuitError.style.display = 'block';
         document.getElementById('address').classList.add('border-danger');
     } else {
         addressError.style.display = 'none';
+        addressBuitError.style.display = 'none';
         document.getElementById('address').classList.remove('border-danger');
     }
 
-    if (!/^[+\d]+$/.test(telefon)) {
+    if (telefon.trim() === '' || telefon.length === 0) {
+        valid = false;
+        telefonErrorLlarg.style.display = 'none';
+        telefonErrorCar.style.display = 'none';
+        telefonBuitError.style.display = 'block';
+        document.getElementById('telefon').classList.add('border-danger');
+    } else if (!/^[+\d]+$/.test(telefon)) {
         valid = false;
         telefonErrorCar.style.display = 'block';
+        telefonBuitError.style.display = 'none';
+        telefonErrorLlarg.style.display = 'none';
         document.getElementById('telefon').classList.add('border-danger');
-    } else if (telefon.length > 15) {
+    }  else if (telefon.length > 15) {
         valid = false;
         telefonErrorLlarg.style.display = 'block';
         telefonErrorCar.style.display = 'none';
+        telefonBuitError.style.display = 'none';
         document.getElementById('telefon').classList.add('border-danger');
     } else {
         telefonErrorLlarg.style.display = 'none';
         telefonErrorCar.style.display = 'none';
+        telefonBuitError.style.display = 'none';
         document.getElementById('telefon').classList.remove('border-danger');
     }
 
